b643f34560f7aa3cf0faea67dc3e5f2723b11057
psh2590 <piyush.ajpandey@gmail.com>
2026-02-05 21:28:07 +0530

    Add Patients component with patient details table

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,1 +1,23 @@
-
+export default function Patients() {
+  return (
+    <>
+      <h1>Patients</h1>
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Mobile</th>
+            <th>Last Visit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Rahul Sharma</td>
+            <td>9876543210</td>
+            <td>02 Feb 2026</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
