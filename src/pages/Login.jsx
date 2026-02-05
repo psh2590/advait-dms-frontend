9ada73c26621fb8facfea1d90886d2054cf80e43
psh2590 <piyush.ajpandey@gmail.com>
2026-02-05 21:27:17 +0530

    Implement login functionality with navigation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const login = () => {
+    navigate("/dashboard");
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1>Advait Prime DMS</h1>
+      <input placeholder="Email" />
+      <input placeholder="Password" type="password" />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "10px",
+  },
+};
 
